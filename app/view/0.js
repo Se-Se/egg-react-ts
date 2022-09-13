b55addde366e177842a74daebbfce9810ee76855
C-B\chen_bo <chenbo919111@126.com>
2022-09-13 08:39:21 +0800

    master init

---
+++ app/view/0.js
@@ -0,0 +1,27 @@
+exports.ids = [0];
+exports.modules = {
+
+/***/ "./app/web/asset/images/react.png":
+/*!****************************************!*\
+  !*** ./app/web/asset/images/react.png ***!
+  \****************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"img/react.png\";\n\n//# sourceURL=webpack:///./app/web/asset/images/react.png?");
+
+/***/ }),
+
+/***/ "./app/web/page/blog/widget/async-image.tsx":
+/*!**************************************************!*\
+  !*** ./app/web/page/blog/widget/async-image.tsx ***!
+  \**************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ \"tslib\");\n/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(tslib__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ \"react\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _asset_images_react_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../asset/images/react.png */ \"./app/web/asset/images/react.png\");\n/* harmony import */ var _asset_images_react_png__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_asset_images_react_png__WEBPACK_IMPORTED_MODULE_2__);\n\n\n\n\nvar AsyncImage = function (_super) {\n  Object(tslib__WEBPACK_IMPORTED_MODULE_0__[\"__extends\"])(AsyncImage, _super);\n\n  function AsyncImage() {\n    return _super !== null && _super.apply(this, arguments) || this;\n  }\n\n  AsyncImage.prototype.render = function () {\n    return react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(\"div\", null, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(\"img\", {\n      src: _asset_images_react_png__WEBPACK_IMPORTED_MODULE_2___default.a\n    }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(\"div\", null, \"\\u8FD9\\u662F\\u5F02\\u6B65\\u52A0\\u8F7D\\u7684\\u5185\\u5BB9\"));\n  };\n\n  return AsyncImage;\n}(react__WEBPACK_IMPORTED_MODULE_1__[\"Component\"]);\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (AsyncImage);\n\n//# sourceURL=webpack:///./app/web/page/blog/widget/async-image.tsx?");
+
+/***/ })
+
+};;
